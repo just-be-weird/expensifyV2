09bfcf6d9f1705e0ba9973ae81f226e5757839c5
abhishek <abhi.chandresh@gmail.com>
2018-11-15 17:19:45 +0530

    Formatted amount and date.

--- src/components/ExpenseListItem.jsx
+++ src/components/ExpenseListItem.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numral from 'numeral';
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (//as dispatch is on props 
     <div>
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>
-        <p>{amount} - {createdAt}</p>
+        <p>
+            {numeral(amount).format('$0,0.00')} - 
+            {moment(createdAt).createdAt('MMMM Do, YYYY')}
+        </p>
     </div>
 );
 
