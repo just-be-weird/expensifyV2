8effdf34340dcb5e80b4d2173def0ce045946618
abhishek <abhi.chandresh@gmail.com>
2018-12-12 23:08:34 +0530

    EditExpensePage, AddExpensePage for test.

--- src/components/EditExpensePage.jsx
+++ src/components/EditExpensePage.jsx
@@ -3,25 +3,37 @@ import { connect } from 'react-redux';
 import { editExpenseAG,removeExpenseAG } from '../actions/expensesAG'
 import ExpenseForm from "./ExpenseForm";
 
-const EditExpensePage = (props) => {
-//get the expense pass it down to expenseform which will set it to state instead of using default props 
-    return (
-        <div>
-            <ExpenseForm 
-                expense = {props.expense}
-                onSubmit={ (expense) => {//refer AddExpensePage
-                //dispatch the action for the edited expense
-                //navigate to the dashboard page
-                    props.dispatch(editExpenseAG( props.expense.id, expense ))
-                    props.history.push('/');
-                }}
-            />
-            <button onClick={(e)=>{
-                props.dispatch(removeExpenseAG( { id: props.expense.id } ));
-                props.history.push('/');
-            }}>x</button>
-        </div>
-    );
+export class EditExpensePage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.onRemove = this.onRemove.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onRemove (e) {
+        this.props.removeExpenseAG({ id: this.props.expense.id });
+        this.props.history.push('/');
+    }
+
+    onSubmit (expense) {//refer AddExpensePage
+        //dispatch the action for the edited expense
+        //navigate to the dashboard page
+            this.props.editExpenseAG(this.props.expense.id, expense);
+            this.props.history.push('/');
+    }
+
+    render() {
+        //get the expense pass it down to expenseform which will set it to state instead of using default props 
+        return (
+            <div>
+                <ExpenseForm 
+                    expense = {this.props.expense}
+                    onSubmit={this.onSubmit}
+                />
+                <button onClick={this.onRemove}>x</button>
+            </div>
+        );
+    }
 }
 
 const mapStoreStateToComponentProps = (state, props) => {// state from redux and props from HOC
@@ -29,4 +41,11 @@ const mapStoreStateToComponentProps = (state, props) => {// state from redux and
         expense: state.expenses.find(( expense ) =>  expense.id === props.match.params.id )
     }
 }
-export default connect(mapStoreStateToComponentProps)(EditExpensePage);
+
+const mapDispatchToComponentProps = (dispatch, props) => {
+    return {
+        editExpenseAG: (id, expense) => dispatch(editExpenseAG(id, expense)),
+        removeExpenseAG: (data) => dispatch(removeExpenseAG(data))
+    }
+}
+export default connect(mapStoreStateToComponentProps, mapDispatchToComponentProps)(EditExpensePage);
---
+++ src/tests/components/EditExpensePage.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import expenses from '../fixtures/expenses';
+import { EditExpensePage } from '../../components/EditExpensePage';
+
+let editExpenseAG, removeExpenseAG, history, wrapper;
+
+beforeEach(() => {
+    editExpenseAG = jest.fn();
+  removeExpenseAG = jest.fn();
+  history = { push: jest.fn() };
+  wrapper = shallow(
+    <EditExpensePage
+      editExpenseAG={editExpenseAG}
+      removeExpenseAG={removeExpenseAG}
+      history={history}
+      expense={expenses[2]}
+    />
+  );
+});
+
+test('should render EditExpensePage', () => {
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('should handle editExpenseAG', () => {
+  wrapper.find('ExpenseForm').prop('onSubmit')(expenses[2]);
+  expect(history.push).toHaveBeenLastCalledWith('/');
+  expect(editExpenseAG).toHaveBeenLastCalledWith(expenses[2].id, expenses[2]);
+});
+
+test('should handle removeExpenseAG', () => {
+  wrapper.find('button').simulate('click');
+  expect(history.push).toHaveBeenLastCalledWith('/');
+  expect(removeExpenseAG).toHaveBeenLastCalledWith({
+    id: expenses[2].id
+  });
+});
