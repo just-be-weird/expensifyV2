e2d7695c902998acbab8ddd35e6370a211775c57
just-be-weird <abhi.chandresh@gmail.com>
2018-12-17 20:21:28 +0530

    setup database environment.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyBlsfeHpt_BlYdbf_48Zfz46MnhN4bRPZQ",
-    authDomain: "expensifyapp-f8e96.firebaseapp.com",
-    databaseURL: "https://expensifyapp-f8e96.firebaseio.com",
-    projectId: "expensifyapp-f8e96",
-    storageBucket: "expensifyapp-f8e96.appspot.com",
-    messagingSenderId: "588822955735"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATBASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 firebase.initializeApp(config);
 const database = firebase.database();
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
 const path = require('path');
+const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin'); // for seperating css from bundle.js to its own file
 
+
+process.env.NODE_ENV = process.NODE_ENV || 'development';
+
+if (process.env.NODE_ENV === 'test') {
+    require('dotenv').config({ path: '.env.test'});
+} else if (process.env.NODE_ENV === 'development') {
+    require('dotenv').config({ path: '.env.development'});
+}
 
 module.exports = (env) => {
     const isProductionEnv = env === 'production';
@@ -40,7 +49,15 @@ module.exports = (env) => {
             ]
         },
         plugins: [
-            CSSExtract
+            CSSExtract,
+            new webpack.DefinePlugin({
+                'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+                'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+                'process.env.FIREBASE_DATBASE_URL': JSON.stringify(process.env.FIREBASE_DATBASE_URL),
+                'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+                'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+                'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringfy(process.env.FIREBASE_MESSAGING_SENDER_ID)
+            })
         ],
         resolve: {
             extensions: ['.js', '.jsx']
